7a0f6547b7774a9fd369c59970e43ac4982092b2
becca <rebecca.kaci@gmail.com>
2021-10-18 15:47:22 +0200

    Exercice 16 jQuery terminé

---
+++ exercice16/js/main.js
@@ -0,0 +1,5 @@
+// Après avoir cliqué sur le bouton magique : ajouter le texte Konexio! dans la boîte rouge avec la méthode html
+
+$("button").click(function(){
+    $("#text").text("Konexio!");
+});
