796e1b5df42e580b7b6a3d1ecc9ef1c4391ad471
becca <rebecca.kaci@gmail.com>
2021-10-18 15:27:29 +0200

    Exercice 11 jQuery terminé

---
+++ exercice11/js/main.js
@@ -0,0 +1,5 @@
+// Après avoir cliqué sur le bouton magique : changer la couleur du carré en rouge avec la méthode css
+
+$("button").click(function(){
+    $("#square").css("background-color", "red");
+  });
