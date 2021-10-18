c66add61632164ac91e4dad556bbc7de5fb8b179
becca <rebecca.kaci@gmail.com>
2021-10-18 15:50:31 +0200

    Exercice 17 jQuery terminé

---
+++ exercice17/js/main.js
@@ -0,0 +1,9 @@
+// Le texte Konexio! est dans le code HTML mais invisible à l'oeil de l'utilisateur. Après avoir cliqué sur le bouton magique : faites le apparaître avec la méthode show. 
+
+$("button").click(function(){
+    $("#text").show(3000, function() {
+    });
+});
+
+// A quelle règle CSS correspond la méthode show ?
+//---> visibility: visible
