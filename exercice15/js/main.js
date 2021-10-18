3cf92a2b9e725302bc983b3040fc7c963440564e
becca <rebecca.kaci@gmail.com>
2021-10-18 15:37:19 +0200

    Exercice 15 jQuery terminé

---
+++ exercice15/js/main.js
@@ -0,0 +1,5 @@
+// Après avoir cliqué sur le bouton magique : ajouter l'attribut class de valeur float-right sur le texte avec la méthode addClass
+
+$("button").click(function(){
+    $("#text").addClass('float-right');
+});
