960a98906ad81c4ce5b57ff01873c684b95f9cca
becca <rebecca.kaci@gmail.com>
2021-10-18 16:17:58 +0200

    Exercice 21 jQuery terminé

--- exercice21/js/main.js
+++ exercice21/js/main.js
@@ -1,2 +1,9 @@
 // Après avoir cliqué sur le bouton magique : animer la largeur du carré à 500 pixels avec la méthode animate
 
+$("button").click(function(){
+    $("#square").animate({
+        width: '+=400px'
+    }, 3000, function() { 
+      console.log("animation terminée !");
+    });
+});
