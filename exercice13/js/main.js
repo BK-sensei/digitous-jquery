6beecbc4609a057c5383fc8718275a31072f9916
becca <rebecca.kaci@gmail.com>
2021-10-18 15:33:09 +0200

    Exercice 13 jQuery terminé

---
+++ exercice13/js/main.js
@@ -0,0 +1,5 @@
+// Après avoir cliqué sur le bouton magique : changer la taille de la police de caractère à 50 pixels avec la méthode css
+
+$("button").click(function () {
+    $(text).css('font-size', "50px");   
+});
--- exercice3/js/main.js
+++ exercice3/js/main.js
@@ -1,3 +1,5 @@
-$(document).click(function () {
+// Changer la taille de la police de caractère à 50 pixels avec la méthode css
+
+$(function () {
     $(text).css('font-size', "50px");   
 });
