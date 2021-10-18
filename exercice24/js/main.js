2112457957baac2c2decb4c8ab69db2bfabb2d35
becca <rebecca.kaci@gmail.com>
2021-10-18 16:37:34 +0200

    Exercice 24 jQuery terminé

---
+++ exercice24/js/main.js
@@ -0,0 +1,8 @@
+// Après avoir cliqué sur le bouton magique : déplacer l'image de 100 pixels vers la droite avec animate
+
+$("button").click(function(){
+    $("#bg").animate({
+        'margin-left': '+=100px',
+    }, 3000, function() { 
+    });
+});
