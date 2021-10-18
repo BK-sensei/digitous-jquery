147d0b3d261b544341558d4b6a53fb56780dd3a9
becca <rebecca.kaci@gmail.com>
2021-10-18 17:31:45 +0200

    Exercice 26 jQuery terminé

--- exercice26/js/main.js
+++ exercice26/js/main.js
@@ -1,2 +1,12 @@
 // Après avoir cliqué sur le bouton magique : ajouter la classe Bootstrap is-invalid à l'input
 
+$(function(){
+
+    $("button").click(function(){
+        $("input").addClass("is-invalid");
+    });
+    
+});
+
+   
+
