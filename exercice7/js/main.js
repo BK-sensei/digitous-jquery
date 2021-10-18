83170b8f50b11719b570efe7dd09a6735afb3d37
becca <rebecca.kaci@gmail.com>
2021-10-18 15:11:59 +0200

    Exercice 7 jQuery terminé

---
+++ exercice7/js/main.js
@@ -0,0 +1,6 @@
+// Le texte Konexio! est dans le code HTML mais invisible à l'oeil de l'utilisateur. Faites le apparaître avec la méthode css
+
+$(function(){
+    $("#text").show(3000, function() {
+    });
+});
